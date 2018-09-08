89421c10cea098f6e0f671b0897cc3fd865cbc4e
HedgehogFog <hedgehog_fogs@mail.ru>
2018-09-08 13:19:59 +0300

    DB & requsting

--- src/main/java/com/example/controller/OnionController.java
+++ src/main/java/com/example/controller/OnionController.java
@@ -1,15 +1,46 @@
 package com.example.controller;
 
+import com.example.model.Onion;
+import com.example.repo.OnionRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/onions")
 public class OnionController {
 
-    @RequestMapping("/user")
-    @ResponseBody
-    String hello() {
-        return "User, hi!";
+    private OnionRepo repository;
+
+    @Autowired
+    public OnionController(OnionRepo repository){
+        this.repository = repository;
     }
-}
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Onion> get(@PathVariable("id") Long id) {
+        Onion onion = repository.findOne(id);
+        if (null == onion) {
+            return new ResponseEntity<Onion>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Onion>(onion, HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/new", method = RequestMethod.POST)
+    public ResponseEntity<Onion> update(@RequestBody Onion onion) {
+        repository.save(onion);
+        return get(onion.getId());
+    }
+
+    @RequestMapping
+    public List<Onion> all() {
+        return repository.findAll();
+    }
+ }
---
+++ src/main/java/com/example/model/Onion.java
@@ -0,0 +1,42 @@
+package com.example.model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Onion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String msg;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
